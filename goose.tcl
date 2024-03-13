d2d222c2717976dd045ca39381e490c82d5c6ab9
Eloi Torrents <eloitor@disroot.org>
2024-03-13 18:04:59 +0100

    add focus next functionality

--- goose.tcl
+++ goose.tcl
@@ -11,21 +11,38 @@ proc bind {path binding action description} {
 	original_bind $path $binding $action
 }
 
+proc focus_next {} {
+	set windows [pack content .frame]
+	set idx [lsearch -exact $windows [focus]]
+	if {$idx != -1} {
+		focus [lindex $windows [expr {($idx + 1) % [llength $windows]}]]
+	}
+}
+
+proc delete_current {} {
+	set current [focus]
+	focus_next
+	pack forget $current
+}
+
+
 bind . <c> {spawn foobar}        "c: Create window"
-bind . <d> {pack forget [focus]} "d: Delete window"
+bind . <d> {delete_current}      "d: Delete window"
+bind . <j> {focus_next}          "j: Focus next window"
 bind . <e> {exit}                "e: Exit"
 
 label .keybindingsInfo -text $bindings_help -anchor n -justify left
-pack .keybindingsInfo -side left -anchor w -fill both
+pack .keybindingsInfo -side left -anchor w -fill y
+
+frame .frame
+pack .frame -fill both -expand 1
 
 namespace eval goose {
-	variable window {}
 	variable next_id 1
 }
 
 proc spawn {command} {
-	set win_name [join ".textBox $goose::next_id" ""]
-	append goose::windows $win_name
+	set win_name ".frame.textBox$goose::next_id"
 	text $win_name -width 10
 	
 	if {[focus] == {.} } {
@@ -34,6 +51,7 @@ proc spawn {command} {
 		pack $win_name -fill both -expand true -side left -after [focus]
 	}
 	
+	# Add conent to the window
 	$win_name insert 1.0 $win_name
 
 	incr goose::next_id
