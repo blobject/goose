c795355eda074231589c4d4bcb23bfd8dc441388
Eloi Torrents <eloitor@disroot.org>
2024-03-26 19:43:47 +0100

    Unsafe code, segmentation fault

--- tinywl.tcl
+++ tinywl.tcl
@@ -2,4 +2,9 @@
 lappend auto_path lib
 package require wlroots
 
-wl_display_create
+set display [wl_display_create]
+puts $display
+set ev [wl_display_get_event_loop $display]
+puts $ev
+set backend [wlr_backend_autocreate $ev 0]
+puts $backend
--- wlroots.tcl
+++ wlroots.tcl
@@ -6,21 +6,31 @@ if {![critcl::compiling]} {
 }
 
 critcl::tcl 8.6
-
+critcl::cflags -DWLR_USE_UNSTABLE
 critcl::cheaders   -I/usr/include
 critcl::clibraries -L/usr/lib/x86_64-linux-gnu
 critcl::clibraries -lwayland-server
+critcl::clibraries -lwlroots
 critcl::include wayland-server-core.h
+critcl::include wlr/backend.h
 
-critcl::cproc wl_display_create {} void {
-    struct wl_display *wl_display;
-    wl_display = wl_display_create();
-    printf("hello world\n");
+critcl::argtype ulong {unsigned long}
+
+critcl::cproc wl_display_create {} {unsigned long} {
+    return (unsigned long) wl_display_create();
+}
+
+critcl::cproc wl_display_get_event_loop {ulong wl_disp} {unsigned long} {
+    return (unsigned long) wl_display_get_event_loop(wl_disp);
+}
+
+critcl::cproc wlr_backend_autocreate {ulong event_loop ulong session} {unsigned long} {
+    return (unsigned long) wlr_backend_autocreate(event_loop, NULL);
 }
 
 critcl::msg -nonewline { Building ...}
 if {![critcl::load]} {
-    error "Building and loading the project failed."
+    error "Building and loading the project failed."c
 }
 
 package provide wlroots 1
