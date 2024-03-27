6e5f1924f41dbf5f20adfc6a409717ae7a0209a5
Eloi Torrents <eloitor@disroot.org>
2024-03-27 11:08:51 +0100

    Fix segmentation fault for wlroots-0.17

--- tinywl.tcl
+++ tinywl.tcl
@@ -4,7 +4,9 @@ package require wlroots
 
 set display [wl_display_create]
 puts $display
-set ev [wl_display_get_event_loop $display]
-puts $ev
-set backend [wlr_backend_autocreate $ev]
+# wlroots 0.18
+# https://gitlab.freedesktop.org/wlroots/wlroots/-/commit/d1b39b58432c471c16e09103fd2c7850e3c41950
+# set ev [wl_display_get_event_loop $display]
+# set backend [wlr_backend_autocreate $ev]
+set backend [wlr_backend_autocreate $display]
 puts $backend
