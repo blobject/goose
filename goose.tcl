5587937be580adfbac8ff4da465eda955d493875
Eloi Torrents <eloitor@disroot.org>
2024-04-16 00:29:56 +0200

    Create backend and renderer

--- goose.tcl
+++ goose.tcl
@@ -3,5 +3,11 @@
 package require Ffidl
 
 ffidl::callout wl_display_create {} pointer [ffidl::symbol libwlroots.so.12 wl_display_create]
+ffidl::callout wlr_backend_autocreate {pointer} pointer [ffidl::symbol libwlroots.so.12 wlr_backend_autocreate]
+ffidl::callout wlr_renderer_autocreate {pointer} pointer [ffidl::symbol libwlroots.so.12 wlr_renderer_autocreate]
+ffidl::callout wlr_renderer_init_wl_display {pointer pointer} int [ffidl::symbol libwlroots.so.12 wlr_renderer_init_wl_display]
 
-wl_display_create
+set display [wl_display_create]
+set backend [wlr_backend_autocreate $display]
+set renderer [wlr_renderer_autocreate $backend]
+puts [wlr_renderer_init_wl_display $renderer $display]
